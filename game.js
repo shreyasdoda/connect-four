6779a7c2815610bc0802bf6c9cc5cdc5114e7416
shreyasdoda <shreyurd@gmail.com>
2020-03-12 12:35:31 +0530

    onmouseover and onmouseout shows red and yelow respectively according to isRed=t/f

---
+++ game.js
@@ -0,0 +1,150 @@
+var isRED = true;
+
+function showTA(){
+    if(isRED){
+        let item = document.getElementById('ra');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('ya');
+        item.style.display = 'block';
+    }
+}
+
+function hideTA(){
+    if(isRED){
+        let item = document.getElementById('ra');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('ya');
+        item.style.display = 'none';
+    }
+}
+
+
+function showTB(){
+    if(isRED){
+        let item = document.getElementById('rb');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('yb');
+        item.style.display = 'block';
+    }
+}
+
+function hideTB(){
+    if(isRED){
+        let item = document.getElementById('rb');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('yb');
+        item.style.display = 'none';
+    }
+}
+
+function showTC(){
+    if(isRED){
+        let item = document.getElementById('rc');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('yc');
+        item.style.display = 'block';
+    }
+}
+
+function hideTC(){
+    if(isRED){
+        let item = document.getElementById('rc');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('yc');
+        item.style.display = 'none';
+    }
+}
+
+
+function showTD(){
+    if(isRED){
+        let item = document.getElementById('rd');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('yd');
+        item.style.display = 'block';
+    }
+}
+
+function hideTD(){
+    if(isRED){
+        let item = document.getElementById('rd');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('yd');
+        item.style.display = 'none';
+    }
+}
+
+function showTE(){
+    if(isRED){
+        let item = document.getElementById('re');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('ye');
+        item.style.display = 'block';
+    }
+}
+
+function hideTE(){
+    if(isRED){
+        let item = document.getElementById('re');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('ye');
+        item.style.display = 'none';
+    }
+}
+
+function showTF(){
+    if(isRED){
+        let item = document.getElementById('rf');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('yf');
+        item.style.display = 'block';
+    }
+}
+
+function hideTF(){
+    if(isRED){
+        let item = document.getElementById('rf');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('yf');
+        item.style.display = 'none';
+    }
+}
+
+function showTG(){
+    if(isRED){
+        let item = document.getElementById('rg');
+        item.style.display = 'block';
+        //alert("SHOULDN'T BE CALLED");
+    } else {
+        let item = document.getElementById('yg');
+        item.style.display = 'block';
+    }
+}
+
+function hideTG(){
+    if(isRED){
+        let item = document.getElementById('rg');
+        item.style.display = 'none';
+    } else {
+        let item = document.getElementById('yg');
+        item.style.display = 'none';
+    }
+}
