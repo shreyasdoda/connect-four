01c0f95971c702932a0cbefa013f4db57b799a23
shreyasdoda <shreyurd@gmail.com>
2020-03-12 17:41:49 +0530

    ->Array updates when clicked ->Color toggles on every move ->Game ends and refreshes when a color wins ->Coins don't move though

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
-var isRED = true;
+// +1 for RED, +2 for YELLOW
+var gameOver = false;
+var isRED = false;
+var dropdiv;
+var layout = [ [0,0,0,0,0,0] , [0,0,0,0,0,0] , [0,0,0,0,0,0] , [0,0,0,0,0,0] , [0,0,0,0,0,0] , [0,0,0,0,0,0] , [0,0,0,0,0,0] ];
+var maxDepth = [ 6 , 6 , 6 , 6 , 6 , 6 , 6 ];
+
 
 function showTA(){
     if(isRED){
@@ -10,7 +16,6 @@ function showTA(){
         item.style.display = 'block';
     }
 }
-
 function hideTA(){
     if(isRED){
         let item = document.getElementById('ra');
@@ -20,7 +25,6 @@ function hideTA(){
         item.style.display = 'none';
     }
 }
-
 
 function showTB(){
     if(isRED){
@@ -32,7 +36,6 @@ function showTB(){
         item.style.display = 'block';
     }
 }
-
 function hideTB(){
     if(isRED){
         let item = document.getElementById('rb');
@@ -53,7 +56,6 @@ function showTC(){
         item.style.display = 'block';
     }
 }
-
 function hideTC(){
     if(isRED){
         let item = document.getElementById('rc');
@@ -63,7 +65,6 @@ function hideTC(){
         item.style.display = 'none';
     }
 }
-
 
 function showTD(){
     if(isRED){
@@ -75,7 +76,6 @@ function showTD(){
         item.style.display = 'block';
     }
 }
-
 function hideTD(){
     if(isRED){
         let item = document.getElementById('rd');
@@ -96,7 +96,6 @@ function showTE(){
         item.style.display = 'block';
     }
 }
-
 function hideTE(){
     if(isRED){
         let item = document.getElementById('re');
@@ -117,7 +116,6 @@ function showTF(){
         item.style.display = 'block';
     }
 }
-
 function hideTF(){
     if(isRED){
         let item = document.getElementById('rf');
@@ -138,7 +136,6 @@ function showTG(){
         item.style.display = 'block';
     }
 }
-
 function hideTG(){
     if(isRED){
         let item = document.getElementById('rg');
@@ -146,5 +143,323 @@ function hideTG(){
     } else {
         let item = document.getElementById('yg');
         item.style.display = 'none';
+    }
+}
+
+function initCoin(dropped){
+    dropped.style.width = '80px';
+    dropped.style.height = '80px';
+    dropped.style.margin = '20px 20px';
+    dropped.style.borderRadius = '50%';
+    dropped.style.display = 'block';
+
+
+    if(isRED){
+        dropped.style.backgroundColor='#ff3d33';
+    } else {
+        dropped.style.backgroundColor='#fffc4f';
+    }
+    
+}
+
+function checkState(){
+    let count;
+
+    //HORIZONTAL CHECK
+    for(let i=0; i<6; i++){
+        count=1;
+        for(let j=0; j<6; j++){
+            if(layout[j][i] == layout[j+1][i]){
+                count++;
+
+                if(count==4){
+                    if(layout[j][i]==2 || layout[j][i]==1){
+                        console.log(layout);
+                        gameOver = true;
+                        if(isRED){
+                            alert("Red is the winner!!!");
+                        } else {
+                            alert("Yellow is the winner!!!");
+                        }
+                        location.reload();
+                    } else {
+                        count = 1;
+                    }
+                }
+            } else {
+                count=1;
+            }
+        }
+    }
+    //VERTICAL CHECK
+    if(!gameOver){
+        for(let i=0; i<7; i++){
+            count=1;
+            for(let j=0; j<5; j++){
+                if(layout[i][j] == layout[i][j+1]){
+                    count++;
+    
+                    if(count==4){
+                        if(layout[i][j]==2 || layout[i][j]==1){
+                            console.log(layout);
+                            gameOver = true;
+                            if(isRED){
+                                alert("Red is the winner!!!");
+                            } else {
+                                alert("Yellow is the winner!!!");
+                            }
+                            location.reload();
+                        } else {
+                            count = 1;
+                        }
+                    }
+                } else {
+                    count=1;
+                }
+            }
+        }
+    }
+    //DIAGONAL CHECK
+    if(!gameOver){
+        for(let i=3; i<=5; i++){
+            let t;
+            let u;
+            count = 1;
+            for(t=0, u=i; t<i, u>0; t++, u--){
+                if(layout[t][u] == layout[t+1][u-1]){
+                    count++;
+
+                    if(count==4){
+                        if(layout[t][u]==2 || layout[t][u]==1){
+                            console.log(layout);
+                            gameOver = true;
+                            if(isRED){
+                                alert("Red is the winner!!!");
+                            } else {
+                                alert("Yellow is the winner!!!");
+                            }
+                            break;
+                        } else {
+                            count = 1;
+                        }
+                    }
+                } else {
+                    count=1;
+                }
+            }
+        }
+    }
+
+
+    if(!gameOver){
+        for(let i=0; i<=2; i++){
+            let t;
+            let u;
+            for(u=i, t=6; u<5, t>i+1; u++, t--){
+                if(layout[t][u] == layout[t-1][u+1]){
+                    count++;
+
+                    if(count==4){
+                        if(layout[t][u]==2 || layout[t][u]==1){
+                            console.log(layout);
+                            gameOver = true;
+                            if(isRED){
+                                alert("Red is the winner!!!");
+                            } else {
+                                alert("Yellow is the winner!!!");
+                            }
+                            //break;
+                            location.reload();
+                        } else {
+                            count = 1;
+                        }
+                    }
+                } else {
+                    count=1;
+                }
+            }
+        }
+    }
+
+    isRED = !isRED;
+    console.log(layout);
+
+}
+
+function alertMaxLimit(){
+    alert("This column is full");
+}
+
+function dropTA(){
+    
+    if(maxDepth[0]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('ta');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[0][maxDepth[0]-1]++;
+        } else {
+            layout[0][maxDepth[0]-1]++;
+            layout[0][maxDepth[0]-1]++;
+        }
+
+        maxDepth[0]--;
+
+        checkState();
+    } else {
+    alertMaxLimit();
+    }
+}
+
+function dropTB(){
+    if(maxDepth[1]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('tb');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[1][maxDepth[1]-1]++;
+        } else {
+            layout[1][maxDepth[1]-1]++;
+            layout[1][maxDepth[1]-1]++;
+        }
+
+        maxDepth[1]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();    
+    }
+}
+
+function dropTC(){
+    
+    if(maxDepth[2]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('tc');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[2][maxDepth[2]-1]++;
+        } else {
+            layout[2][maxDepth[2]-1]++;
+            layout[2][maxDepth[2]-1]++;
+        }
+
+        maxDepth[2]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();
+    }    
+}
+
+function dropTD(){
+    
+    if(maxDepth[3]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('td');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[3][maxDepth[3]-1]++;
+        } else {
+            layout[3][maxDepth[3]-1]++;
+            layout[3][maxDepth[3]-1]++;
+        }
+
+        maxDepth[3]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();
+    }
+}
+
+function dropTE(){
+    
+    if(maxDepth[4]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('te');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[4][maxDepth[4]-1]++;
+        } else {
+            layout[4][maxDepth[4]-1]++;
+            layout[4][maxDepth[4]-1]++;
+        }
+
+        maxDepth[4]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();
+    }
+}
+
+function dropTF(){
+    
+    if(maxDepth[5]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('tf');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[5][maxDepth[5]-1]++;
+        } else {
+            layout[5][maxDepth[5]-1]++;
+            layout[5][maxDepth[5]-1]++;
+        }
+
+        maxDepth[5]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();
+    }
+}
+
+function dropTG(){
+    
+    if(maxDepth[6]>0){
+        dropdiv = document.createElement("div");
+        initCoin(dropdiv);
+
+        let parent = document.getElementById('tg');
+        parent.appendChild(dropdiv);
+
+
+        if(isRED){
+            layout[6][maxDepth[6]-1]++;
+        } else {
+            layout[6][maxDepth[6]-1]++;
+            layout[6][maxDepth[6]-1]++;
+        }
+
+        maxDepth[6]--;
+
+        checkState();
+    } else {
+        alertMaxLimit();
     }
 }
---
+++ test.js
@@ -0,0 +1,8 @@
+function a(){
+    alert("CLICKED");
+}
+
+var items = [[0,0],[1,4]];
+
+items[1][1]++;
+console.log(items);
